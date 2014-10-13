a6afba8eecb57863b4563cabacd478836191b684
Guy Matz <gmatz@mdsol.com>
2014-10-13 10:31:19 -0400

    added ability to load custom filters from filters dir

--- parser.py
+++ parser.py
@@ -3,9 +3,25 @@ from flask import Flask, render_template, request
 from jinja2 import Template, Environment, meta
 from random import choice
 import json
-
+# For dynamic loading of filters
+import imp
+from inspect import getmembers, isfunction
+import os
 
 app = Flask(__name__)
+
+# Load filters in filters dir
+filter_path='filters'
+for filter in os.listdir(filter_path):
+    if not os.path.isfile(os.path.join(filter_path, filter)):
+        continue
+    else:
+        mod_name,file_ext = os.path.splitext(os.path.split(filter)[-1])
+        if file_ext.lower() == '.py':
+            py_mod = imp.load_source(mod_name, os.path.join(filter_path, filter))
+            for name, function in getmembers(py_mod):
+                    if isfunction(function):
+                        app.jinja_env.filters[name] = function
 
 @app.route("/")
 def hello():
