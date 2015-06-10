ff0ae1fe6a345c08fdb4d1c2b252ef20f9195efa
Sherif Zain <sherif@falconsocial.com>
2015-06-10 16:18:59 +0200

    Removed: dummy values checkbox and added a message in case of error parsing json

--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return render_template('index.html')
@@ -14,27 +15,18 @@ def hello():
 
 @app.route('/convert', methods=['GET', 'POST'])
 def convert():
-    dummy_values = [ 'Lorem', 'Ipsum', 'Amet', 'Elit', 'Expositum', 
-        'Dissimile', 'Superiori', 'Laboro', 'Torquate', 'sunt', 
-    ]
-
-    tpl = Template(request.form['template'])
-    values = {}
-
-    if bool(int(request.form['dummyvalues'])):
-        # List variables (introspection)
-        env = Environment()
-        vars_to_fill = meta.find_undeclared_variables(env.parse(request.form['template']))
-
-        for v in vars_to_fill:
-            values[v] = choice(dummy_values)
-    else:
-        values = json.loads(request.form['values'])
+    tpl = Template(request.form.get('template'))
+    values = request.form.get('values')
+    try:
+        values = json.loads(values) if values else {}
+    except:
+        return 'Invalid JSON!'
 
     rendered_tpl = tpl.render(values)
 
     if bool(int(request.form['showwhitespaces'])):
-        # Replace whitespaces with a visible character (will be grayed with javascript)
+        # Replace whitespaces with a visible character (will be grayed with
+        # javascript)
         rendered_tpl = rendered_tpl.replace(' ', u'•')
 
     return rendered_tpl.replace('\n', '<br />')
@@ -42,4 +34,4 @@ def convert():
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
