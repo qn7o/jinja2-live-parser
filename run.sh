64d4e98c00425178c8a89c1756d29277c61b6974
Guy Matz <guy.matz.cft@jcrew.com>
2015-06-26 09:58:06 -0400

    This really should be in the form of an init script

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Edit this to the location of your jinja2-live parser code
-JLP_HOME=~gmatz/Code/jinja2-live-parser
+#JLP_HOME=~gmatz/Code/jinja2-live-parser
 
-cd $JLP_HOME
-exec python parser.py
+#cd $JLP_HOME
+#exec python parser.py
+virtualenv venv
+source ./venv/bin/activate
+pip install -r requirements.txt
+ln -s ../venv/lib/python2.7/site-packages/ansible/runner/filter_plugins filters/ansible
+python parser.py
