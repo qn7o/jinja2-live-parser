573106bebb882cbdce2633898374ea99e47ed63a
Lars Krieger <lars.krieger@gmail.com>
2015-06-26 10:25:02 +0200

    Cleaned up convert.js

--- static/js/convert.js
+++ static/js/convert.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
     $('#clear').click(function() {
         $('#render').val('');
-        $('#values').val('');
         $('#render').html('');
     });
 
     $('#convert').click(function() {
         var is_checked_showwhitespaces = $('input[name="showwhitespaces"]').is(':checked') ? 1:0;
+        
+        // Lowlevel way to get the content from our editor
+        var jsonText = $('.ace_content').text()
+
         // Push the input to the Jinja2 api (Python)
         $.post(window.location, {
-            values: $('#values').val(),
+            values: jsonText,
             showwhitespaces: is_checked_showwhitespaces
         }).done(function(response) {
             // Grey out the white spaces chars if any
