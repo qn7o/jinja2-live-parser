841fb9db9a942c04e702d5b185af887e092a164d
Lars Krieger <lars.krieger@gmail.com>
2015-09-02 17:01:03 +0200

    Fixed rendering issue missing key for request

--- static/js/common/renderPreview.factory.js
+++ static/js/common/renderPreview.factory.js
@@ -1,19 +1,15 @@
 angular.module('Falcon-email').factory('previewRenderSrvc', function ($http) {
 
 	var html = ''
-
+	
 	return {
 		render: function (data) {
 			return $http({
 				url: window.location,
 				method: 'POST',
-				transformRequest: function(obj) {
-			        var str = [];
-			        for(var p in obj)
-			        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(JSON.stringify(obj[p])));
-			        return str.join("&");
-			    },
-				data: data,
+				data: {
+					data: data
+				},
 				headers: {'Content-Type': 'application/json'},
 			})
 			.then(function (response) {
